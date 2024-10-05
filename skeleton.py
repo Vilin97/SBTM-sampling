8f432a36f0569458b3f6e0da2fdc47ee6d6a573c
Vas <vasilin97@gmail.com>
2024-10-05 14:50:17 -0700

    Add class descriptions to the skeleton.

--- skeleton.py
+++ skeleton.py
@@ -6,10 +6,12 @@ What do I want from the code?
 - compare different NN architectures for SBTM: MLP, ResNet, transformer
 - compare different numbers of SGD steps for SBTM
 - compare different loss functions for SBTM
-- compute and plot various metrics: Fisher divergence, KL divergence, moments, score approximation error, etc.
-- save results and intermediate states and run analysis afterwards
+
 - pre-train initial NNs and save them
+- save results and intermediate states and run analysis afterwards
+- compute and plot various metrics: Fisher divergence, KL divergence, moments, score approximation error, etc.
 - record lots of things: particle positions, NN weights, etc.
+
 - everything should be on the GPU
 - use Flax for NNs
 """
@@ -20,11 +22,14 @@ class Sampler:
     target_score # target score function
     step_size # delta t
     max_steps # maximum number of steps
+    logger # logger object
 
     def sample(self):
         """Sample from the target distribution"""
         for _ in range(self.max_steps):
             self.step()
+            logger.log(...)
+
         return self.particles
 
     def step(self):
@@ -51,6 +56,16 @@ class ODESampler(Sampler):
 
 class SBTMSampler(ODESampler):
     """Use a NN to approximate the score"""
+    # assume that the score model has already been pre-trained
 
 class SVGDSampler(ODESampler):
     """Use RKHS to approximate the score"""
+
+# ScoreModel class to approximate the score function. It should have methods `fit` and `predict`. `predict` is the forward pass of the model. `fit` should fit the model given particle locations. The class is inherited by NNScoreModel and RKHSScoreModel. NNScoreModel should contain a Flax model, a loss, an optimizer, and a callable `stop_gd` that stops the gradient descent (it can perform a fixed number of GD steps or implement a more sophisticated stopping criterion). RKHSScoreModel should contain the kernel function.
+
+# Logger class to log target score, step_size, max-steps, step number, particle locations, predicted score, score model. Should have method `log`.
+
+# Experiment class to orchestrate running a single experiment. It should take a `config` dict. It should have methods `run` and `compute_metrics`. `run` should load the model (or train a new one if not available), init the sampler, run the sampler, and save the config and the results. `compute_metrics` should load the results, compute metrics and save the metrics. `plot_metrics` should load the metrics, plot them, and save the plots. 
+
+# DataManager class to save and load data. Should save and load experiment data and plots. Not sure how to imlpement this yet. Maybe with pickle or maybe with a PostgreSQL database or maybe with pandas.
+
