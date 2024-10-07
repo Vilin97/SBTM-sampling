661f60f42368d7d87c5a23dd2f287dc351bceeb4
Vas <vasilin97@gmail.com>
2024-10-07 15:24:05 -0700

    Expand the skeleton.

--- skeleton.py
+++ skeleton.py
@@ -16,7 +16,7 @@ What do I want from the code?
 - use Flax for NNs
 """
 
-# Classes 
+# Main class to sample
 class Sampler:
     particles # jnp array of particles
     target_score # target score function
@@ -41,7 +41,8 @@ class SDESampler(Sampler):
     """Langevin dynamics sampler"""
     
     def step(self):
-        """Draw brownian noise Z and move particles in direction target_score - sqrt(2)Z"""        
+        """Draw brownian noise Z and move particles in direction target_score - sqrt(2)Z"""
+        particles += step_size * target_score(particles) + sqrt(2 * step_size) * random.normal()
         pass
 
 
@@ -62,10 +63,74 @@ class SVGDSampler(ODESampler):
     """Use RKHS to approximate the score"""
 
 # ScoreModel class to approximate the score function. It should have methods `fit` and `predict`. `predict` is the forward pass of the model. `fit` should fit the model given particle locations. The class is inherited by NNScoreModel and RKHSScoreModel. NNScoreModel should contain a Flax model, a loss, an optimizer, and a callable `stop_gd` that stops the gradient descent (it can perform a fixed number of GD steps or implement a more sophisticated stopping criterion). RKHSScoreModel should contain the kernel function.
+class ScoreModel:
+    def fit(self, particles):
+        raise NotImplementedError("must be implemented by subclasses")
+
+    def predict(self, particles):
+        raise NotImplementedError("must be implemented by subclasses")
+
+
+class NNScoreModel(ScoreModel):
+    def __init__(self, model, loss, optimizer, stop_gd):
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+        self.stop_gd = stop_gd
+
+    def fit(self, particles):
+        pass
+
+    def predict(self, particles):
+        pass
+
+
+class RKHSScoreModel(ScoreModel):
+    def __init__(self, kernel_function):
+        self.kernel_function = kernel_function
+
+    def fit(self, particles):
+        pass
+
+    def predict(self, particles):
+        pass
+
 
 # Logger class to log target score, step_size, max-steps, step number, particle locations, predicted score, score model. Should have method `log`.
+class Logger:
+    def log(self, **kwargs):
+        pass
+
 
 # Experiment class to orchestrate running a single experiment. It should take a `config` dict. It should have methods `run` and `compute_metrics`. `run` should load the model (or train a new one if not available), init the sampler, run the sampler, and save the config and the results. `compute_metrics` should load the results, compute metrics and save the metrics. `plot_metrics` should load the metrics, plot them, and save the plots. 
+class Experiment:
+    def __init__(self, config):
+        self.config = config
 
-# DataManager class to save and load data. Should save and load experiment data and plots. Not sure how to imlpement this yet. Maybe with pickle or maybe with a PostgreSQL database or maybe with pandas.
+    def run(self):
+        pass
+
+    def compute_metrics(self):
+        pass
+
+    def plot_metrics(self):
+        pass
+
+
+# DataManager class to save and load data. Should save and load experiment data and plots. Not sure how to imlpement this yet. Maybe with pickle or maybe with a PostgreSQL database or maybe with pandas. Or maybe with Weights and Biases!
+class DataManager:
+    def save_data(self, data, filename):
+        pass
+
+    def load_data(self, filename):
+        pass
+
+    def save_plot(self, plot, filename):
+        pass
+
+    def load_plot(self, filename):
+        pass
+
+
+
 
