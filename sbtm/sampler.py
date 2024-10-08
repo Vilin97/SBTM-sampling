973cde841ef882d3056cc0ae1302ae0e2c1d0113
Vas <vasilin97@gmail.com>
2024-10-08 10:38:02 -0700

    Start implementing SVGD sampler.

--- sbtm/sampler.py
+++ sbtm/sampler.py
@@ -1,13 +1,34 @@
 import jax.numpy as jnp
 import jax.random as jrandom
+import jax
+import equinox as eqx
+from copy import deepcopy
+from tqdm import tqdm
+
+
+class Logger:
+    """Logger class to log target score, step_size, max-steps, step number, particle locations, predicted score, score model. Should have method `log`."""
+
+    def __init__(self):
+        self.logs = []
+
+    def log(self, **kwargs):
+        self.logs.append(kwargs)
+
 
 class Sampler:
+    particles: jnp.array   # transported particles
+    target_score: callable # target score function
+    step_size: float       # time discretization
+    max_steps: int         # maximum number of steps
+    logger: Logger         # logger object
+
     def __init__(self, particles, target_score, step_size, max_steps, logger):
-        self.particles = particles  # jnp array of particles
-        self.target_score = target_score  # target score function
-        self.step_size = step_size  # delta t
-        self.max_steps = max_steps  # maximum number of steps
-        self.logger = logger  # logger object
+        self.particles = particles
+        self.target_score = target_score  
+        self.step_size = step_size
+        self.max_steps = max_steps
+        self.logger = logger
 
     def sample(self):
         """Sample from the target distribution"""
@@ -41,18 +62,13 @@ class SDESampler(Sampler):
 class ODESampler(Sampler):
     """Deterministic sampler"""
 
+class SBTMSampler(Sampler):
+    """Use a NN to approximate the score"""
+
     def __init__(self, particles, target_score, step_size, max_steps, logger, score_model):
         super().__init__(particles, target_score, step_size, max_steps, logger)
         self.score_model = score_model  # a model to approximate grad-log-density, e.g. a NN
-
-    def step(self):
-        """Train the score model and move particles in direction target_score - score_model(particles)"""
-        raise NotImplementedError("must be implemented by subclasses")
-
-
-class SBTMSampler(ODESampler):
-    """Use a NN to approximate the score"""
-
+    
     # assume that the score model has already been pre-trained
     # TODO: implement SBTM
 
@@ -60,16 +76,62 @@ class SBTMSampler(ODESampler):
 class SVGDSampler(ODESampler):
     """Use RKHS to approximate the velocity (target_score - score)"""
 
-    # TODO: implement SVGD
+    kernel_obj: eqx.Module
 
+    def __init__(self, kernel):
+        self.kernel_obj = kernel
 
-class Logger:
-    """Logger class to log target score, step_size, max-steps, step number, particle locations, predicted score, score model. Should have method `log`."""
+    @eqx.filter_jit
+    def calculate_gradient(self, score_obj, particles, kernel_params=None):
+        num_particles = particles.shape[0]
+        gram_matrix = self.kernel_obj(particles, particles, kernel_params)
+        score_matrix = score_obj(particles)
+        kernel_gradient = self.kernel_obj.gradient_wrt_first_arg(particles,
+                                                                 particles,
+                                                                 kernel_params)
 
-    def __init__(self):
-        self.logs = []
+        # calculate the terms
+        kernel_score_term = jnp.einsum('ij,jk->ik', gram_matrix, score_matrix)
+        kernel_gradient_term = jnp.sum(kernel_gradient, axis=0)
+        phi = (kernel_score_term + kernel_gradient_term) / num_particles
+        return phi
+    
+    @eqx.filter_jit
+    def calculate_length_scale(self, particles):
+        pairwise_sq_distances= jax.vmap(
+            lambda x1: jax.vmap(lambda y1: jnp.sum((x1 - y1) ** 2))(
+                particles))(particles)
+        median_distance = jnp.sqrt(jnp.median(pairwise_sq_distances))
+        new_length_scale = median_distance**2 / jnp.log(len(particles))
+        return new_length_scale
 
-    def log(self, **kwargs):
-        self.logs.append(kwargs)
+    @eqx.filter_jit
+    def update(self, particles, score_obj, step_size, kernel_params=None):
+        gradient = self.calculate_gradient(score_obj, particles, kernel_params)
+        # print(f'Gradient: {jnp.linalg.norm(gradient)}')
+        updated_particles = particles + step_size * gradient
+        return updated_particles
 
+    def predict(self, particles, score_obj, num_iterations, step_size,
+                trajectory=False, adapt_length_scale=False):
+        particle_trajectory = np.zeros((num_iterations + 1, particles.shape[0],
+                              particles.shape[1]))
+        start = deepcopy(particles)
+        particle_trajectory[0] = start
+        kernel_params = self.kernel_obj.params # initialize
 
+        for i in tqdm(range(1, num_iterations + 1)):
+            if 'length_scale' in kernel_params and adapt_length_scale:
+                # Update length scale outside of JIT
+                kernel_params['length_scale'] = self.calculate_length_scale(start)
+            # print(f'Kernel params: {kernel_params}')
+            start = self.update(start, score_obj, step_size, kernel_params)
+            particle_trajectory[i] = start
+
+        if trajectory:
+            return particle_trajectory[-1], particle_trajectory
+
+        else:
+            return particle_trajectory[-1]
+
+    # TODO: write the step method
