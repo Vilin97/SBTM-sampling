8c2300967e7a01766d76ff773f208138da2d36e0
Vas <vasilin97@gmail.com>
2024-12-08 00:06:21 -0800

    Add adaptive time stepping.

--- sbtm/sampler.py
+++ sbtm/sampler.py
@@ -22,16 +22,28 @@ class Logger:
     def log_hyperparameters(self, hyperparameters):
         self.hyperparameters.update(hyperparameters)
 
+# TODO: adaptive stepping: step_size ~ 1/|∇ log π(x)|^2
+class State:
+    def __init__(self, particles):
+        self.particles = particles
+        self.t = 0
+        self.step_number = 0
+
+    def update(self, particles, step_size):
+        self.particles = particles
+        self.t += step_size
+        self.step_number += 1
 
 class Sampler:
-    particles: jnp.array  # transported particles
+    state: State  # state object
     target_score: callable  # target score function
     step_sizes: float  # time discretization
     max_steps: int  # maximum number of steps
     logger: Logger  # logger object
+    t_end: float  # end time
 
-    def __init__(self, particles, target_score, step_sizes, max_steps, logger):
-        self.particles = particles
+    def __init__(self, particles, target_score, step_sizes, max_steps, logger, t_end=None):
+        self.state = State(particles)
         try:
             target_score(particles)
             self.target_score = lambda t, x: target_score(x)
@@ -41,45 +53,61 @@ class Sampler:
         self.max_steps = max_steps
         self.logger = logger
         self.logger.log_hyperparameters({'step_sizes': self.step_sizes, 'max_steps': self.max_steps})
+        if t_end:
+            self.t_end = t_end
+        elif max_steps and step_sizes:
+            self.t_end = max_steps * jnp.sum(step_sizes)
+        else:
+            self.t_end = jnp.inf
 
     def sample(self):
         """Sample from the target distribution"""
-        for step_number, step_size in enumerate(tqdm(self.step_sizes, desc="Sampling")):
-            if step_number == self.max_steps:
+        for step_number in tqdm(range(self.max_steps), desc="Sampling"):
+            if step_number == self.max_steps or self.state.t >= self.t_end:
                 break
-            to_log = {'particles': self.particles, 'step_number': step_number, 'step_size': step_size}
+            step_size = self.step_size(step_number)
+            to_log = {'particles': self.state.particles, 'step_number': step_number, 'step_size': step_size, 'time': self.state.t}
             step_log = self.step(step_number)
             to_log.update(step_log)
             self.logger.log(to_log)
-            if jnp.isnan(self.particles).any():
+            self.state.update(self.state.particles, step_size)
+            if jnp.isnan(self.state.particles).any():
                 raise ValueError(f"Instability detected at step {step_number}")
 
-        to_log = {'particles': self.particles, 'step_number': step_number+1, 'step_size': step_size}
+        to_log = {'particles': self.state.particles, 'step_number': step_number+1, 'step_size': step_size}
         self.logger.log(to_log)
-        return self.particles
+        return self.state.particles
 
     def step(self):
         """Take one step, e.g. predict score and move particles. Return a dictionary of values to log."""
         raise NotImplementedError("must be implemented by subclasses")
 
 
+    def step_size(self, step_number, factor=1e-1):
+        """Return the step size for the given step number"""
+        if self.step_sizes:
+            return self.step_sizes[step_number]
+        else: # 1/E(|∇ log π(x)|^2)
+            return factor * self.state.particles.shape[0] / jnp.sum(jnp.square(self.target_score(self.state.t, self.state.particles)))
+
+
 class SDESampler(Sampler):
     """Langevin dynamics sampler"""
 
-    def __init__(self, particles, target_score, step_sizes, max_steps, logger, seed=0):
-        super().__init__(particles, target_score, step_sizes, max_steps, logger)
+    def __init__(self, particles, target_score, step_sizes, max_steps, logger, seed=0, **kwargs):
+        super().__init__(particles, target_score, step_sizes, max_steps, logger, **kwargs)
         self.key = jrandom.key(seed)
         self.logger.log_hyperparameters({'seed': seed})
 
     def step(self, step_number):
         """Equation (4) in https://arxiv.org/pdf/1907.05600"""
         self.key, subkey = jrandom.split(self.key)
-        n, dim = self.particles.shape
+        n, dim = self.state.particles.shape
         noise = jrandom.multivariate_normal(subkey, jnp.zeros((n, dim)), jnp.eye(dim))
-        t = step_number * self.step_sizes[step_number]
-        drift = self.target_score(t, self.particles)
-        velocity = self.step_sizes[step_number] * drift + jnp.sqrt(2 * self.step_sizes[step_number]) * noise
-        self.particles += velocity
+        t = self.state.t
+        drift = self.target_score(t, self.state.particles)
+        velocity = self.step_size(step_number) * drift + jnp.sqrt(2 * self.step_size(step_number)) * noise
+        self.state.particles += velocity
         return {'noise': noise, 'velocity': velocity}
 
 
@@ -138,37 +166,37 @@ class SBTMSampler(ODESampler):
     def step(self, step_number):
         """Lines 4,5 of algorithm 1 in https://arxiv.org/pdf/2206.04642"""
         loss_values, batch_loss_values = self.train_model()
-        score = self.score_model(self.particles)
-        t = step_number * self.step_sizes[step_number]
+        score = self.score_model(self.state.particles)
+        t = self.state.t
         if self.heun:
-            drift = self.target_score(t, self.particles) - score
-            intermediate_particles = self.particles + self.step_sizes[step_number] * drift
+            drift = self.target_score(t, self.state.particles) - score
+            intermediate_particles = self.state.particles + self.step_size(step_number) * drift
             intermediate_score = self.score_model(intermediate_particles)
             intermediate_drift = self.target_score(t, intermediate_particles) - intermediate_score
-            velocity = self.step_sizes[step_number] * 0.5 * (drift + intermediate_drift)
+            velocity = self.step_size(step_number) * 0.5 * (drift + intermediate_drift)
         else:
-            drift = self.target_score(t, self.particles) - score
-            velocity = self.step_sizes[step_number] * drift
-        self.particles += velocity
+            drift = self.target_score(t, self.state.particles) - score
+            velocity = self.step_size(step_number) * drift
+        self.state.particles += velocity
         return {'score': score, 'velocity': velocity, 'loss_values': loss_values, 'batch_loss_values': batch_loss_values}
 
     def train_model(self):
         """Train the score model using mini-batches"""
         batch_loss_values = []
         loss_values = []
-        num_particles = self.particles.shape[0]
+        num_particles = self.state.particles.shape[0]
         num_batches = num_particles // self.mini_batch_size
 
         while not self.gd_stopping_criterion(loss_values, batch_loss_values):
-            loss_values.append(self.loss(self.score_model, self.particles))
+            loss_values.append(self.loss(self.score_model, self.state.particles))
             for i in range(num_batches):
                 batch_start = i * self.mini_batch_size
                 batch_end = batch_start + self.mini_batch_size
-                batch = self.particles[batch_start:batch_end, :]
+                batch = self.state.particles[batch_start:batch_end, :]
                 loss_value = opt_step(self.score_model, self.optimizer, self.loss, batch)
                 batch_loss_values.append(loss_value)
         
-        loss_values.append(self.loss(self.score_model, self.particles))
+        loss_values.append(self.loss(self.score_model, self.state.particles))
         return loss_values, batch_loss_values
 
 @nnx.jit(static_argnames='loss')
@@ -193,21 +221,21 @@ class SVGDSampler(ODESampler):
         self.logger.log_hyperparameters({'kernel_obj': kernel_obj, 'kernel_width': kernel_width})
 
     def step(self, step_number):
-        width = self.compute_kernel_width(self.particles)
+        width = self.compute_kernel_width(self.state.particles)
         gradient = self.gradient(width)
-        velocity = self.step_sizes[step_number] * gradient
-        self.particles += velocity
+        velocity = self.step_size(step_number) * gradient
+        self.state.particles += velocity
         return {'kernel_width': float(width), 'velocity': velocity}
     
     @eqx.filter_jit
     def gradient(self, width):
         """Equation (8) in https://arxiv.org/pdf/1608.04471"""
-        particles = self.particles
+        particles = self.state.particles
 
         num_particles = particles.shape[0]
         gram_matrix = self.kernel_obj(particles, particles, width)  # k(xⱼ, xᵢ)
         kernel_gradient = self.kernel_obj.gradient_wrt_first_arg(particles, particles, width)  # ∇ⱼ log k(xⱼ, xᵢ)
-        score_matrix = self.target_score(step_number*self.step_sizes[step_number], particles)  # ∇ log π(x)
+        score_matrix = self.target_score(self.state.t, particles)  # ∇ log π(x)
 
         kernel_score_term = jnp.einsum("ij,jk->ik", gram_matrix, score_matrix)  # ∑ⱼ k(xⱼ, xᵢ) ⋅ ∇ log π(xⱼ)
         kernel_gradient_term = jnp.sum(kernel_gradient, axis=0)  # ∑ⱼ ∇ⱼ log k(xⱼ, xᵢ)
